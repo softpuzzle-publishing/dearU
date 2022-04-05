5ffe453cd20ff06c5815e4c39597b81debaa306a
jang <jang0ha@gmail.com>
2022-04-05 10:00:40 +0900

    no message

--- dist/assets/js/ui.js
+++ dist/assets/js/ui.js
@@ -2,20 +2,20 @@
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var html = document.querySelector('html');
-var body = document.querySelector('body');
+var html = document.querySelector("html");
+var body = document.querySelector("body");
 var Header = {
   init: function init() {
     this.menu();
 
-    if ($('h2.title').length > 0) {
-      $('title').text($('h2.title').text() + ' | Lysn store 관리자');
+    if ($("h2.title").length > 0) {
+      $("title").text($("h2.title").text() + " | Lysn store 관리자");
     }
   },
   menu: function menu(e) {
-    $('.btn-hamburger').on('click', function (e) {
+    $(".btn-hamburger").on("click", function (e) {
       e.preventDefault();
-      $('html').toggleClass('is-collapsed');
+      $("html").toggleClass("is-collapsed");
     });
   }
 };
@@ -32,28 +32,28 @@ var Aside = {
   },
   lnb: function lnb() {
     //페이지 타이틀명과 비교하여 활성화
-    if ($('.sidebar').length > 0) {
-      var title = $('h2.title').text();
-      var $active = '';
-      var $activeDep1 = '';
-      $('.sidebar a').each(function () {
+    if ($(".sidebar").length > 0) {
+      var title = $("h2.title").text();
+      var $active = "";
+      var $activeDep1 = "";
+      $(".sidebar a").each(function () {
         if ($(this).text() == title) {
           $active = $(this);
-          $activeDep1 = $(this).closest('.dep1');
+          $activeDep1 = $(this).closest(".dep1");
         }
       });
       $activeDep1.show();
-      $('.gnb a').each(function () {
-        if ($(this).attr('data-gnb-code') == $activeDep1.attr('data-gnb-code')) {
-          $(this).parent('li').addClass('active');
+      $(".gnb a").each(function () {
+        if ($(this).attr("data-gnb-code") == $activeDep1.attr("data-gnb-code")) {
+          $(this).parent("li").addClass("active");
         }
       });
-      $active.parents('li').addClass('active');
-      $active.parents('.has-treeview').addClass('open');
-      $('.dep1').css('opacity', '1');
-      $('.sidebar .has-treeview > a').on('click', function (e) {
+      $active.parents("li").addClass("active");
+      $active.parents(".has-treeview").addClass("open");
+      $(".dep1").css("opacity", "1");
+      $(".sidebar .has-treeview > a").on("click", function (e) {
         e.preventDefault();
-        $(this).closest('li').toggleClass('open');
+        $(this).closest("li").toggleClass("open");
       });
     }
   }
@@ -66,29 +66,29 @@ var Common = {
     this.datePicker();
     this.timePicker();
     this.event();
-    window.addEventListener('mousewheel', Common.scrolling);
-    window.addEventListener('touchmove', Common.scrolling);
+    window.addEventListener("mousewheel", Common.scrolling);
+    window.addEventListener("touchmove", Common.scrolling);
     $(window).scroll(function () {
       Common.scrolling();
     });
   },
   scrolling: function scrolling(e) {
     var scrollTop = $(window).scrollTop();
-    var gnbTop = $('#header').height();
+    var gnbTop = $("#header").height();
     gnbTop = Number(gnbTop);
 
     if (scrollTop > 0) {
-      $('html').addClass('is-scrolled');
+      $("html").addClass("is-scrolled");
     } else {
-      $('html').removeClass('is-scrolled');
+      $("html").removeClass("is-scrolled");
     } //scrollbar bottom check
 
 
-    if ($('html').hasClass('is-scrolled')) {
+    if ($("html").hasClass("is-scrolled")) {
       if ($(window).scrollTop() + $(window).height() > $(document).height() - 50) {
-        $('html').addClass('is-bottom');
+        $("html").addClass("is-bottom");
       } else {
-        $('html').removeClass('is-bottom');
+        $("html").removeClass("is-bottom");
       }
     }
   },
@@ -98,16 +98,16 @@ var Common = {
 
     if ($('[id^="split-horizontal"]').length > 0) {
       $('[id^="split-horizontal"]').each(function () {
-        var item = '#' + $(this).attr('id');
+        var item = "#" + $(this).attr("id");
         horizontalItems.push(item);
       });
       splitH = Split(horizontalItems, {
-        direction: 'horizontal',
+        direction: "horizontal",
         gutterSize: 8,
         minSize: 435,
         snapOffset: 0,
         onDrag: function onDrag() {
-          window.dispatchEvent(new Event('resize'));
+          window.dispatchEvent(new Event("resize"));
         }
       });
     }
@@ -117,7 +117,7 @@ var Common = {
 
     //datepicker
     var currentDate = new Date();
-    $('.form-datepicker').datepicker((_$$datepicker = {
+    $(".form-datepicker").datepicker((_$$datepicker = {
       defaultDate: +7,
       changeMonth: true,
       changeYear: true,
@@ -127,15 +127,15 @@ var Common = {
       showMonthAfterYear: true,
       showOtherMonths: true
     }, _defineProperty(_$$datepicker, "changeMonth", true), _defineProperty(_$$datepicker, "changeYear", true), _defineProperty(_$$datepicker, "dateFormat", "yy-mm-dd"), _defineProperty(_$$datepicker, "gotoCurrent", true), _defineProperty(_$$datepicker, "beforeShow", function beforeShow(input, inst) {
-      $('#ui-datepicker-div').addClass('datepicker-box');
-    }), _$$datepicker)).datepicker('setDate', 'today');
+      $("#ui-datepicker-div").addClass("datepicker-box");
+    }), _$$datepicker)).datepicker("setDate", "today");
   },
   timePicker: function timePicker() {
     //timepicker
-    $('.form-timepicker').each(function () {
+    $(".form-timepicker").each(function () {
       $(this).timepicker({
         showMeridian: false,
-        defaultTime: '00:00'
+        defaultTime: "00:00"
       });
     });
   },
@@ -149,48 +149,48 @@ var Common = {
       theme: "dark"
     }); //모달 중첩 z-index 조정
 
-    $('.modal').on('show.bs.modal', function (e) {
-      var zIndex = 1040 + 10 * $('.modal:visible').length;
-      $(this).css('z-index', zIndex);
+    $(".modal").on("show.bs.modal", function (e) {
+      var zIndex = 1040 + 10 * $(".modal:visible").length;
+      $(this).css("z-index", zIndex);
       setTimeout(function () {
-        $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+        $(".modal-backdrop").not(".modal-stack").css("z-index", zIndex - 1).addClass("modal-stack");
       }, 0);
-    }).on('hidden.bs.modal', function () {
-      if ($('.modal:visible').length > 0) {
+    }).on("hidden.bs.modal", function () {
+      if ($(".modal:visible").length > 0) {
         setTimeout(function () {
-          $(document.body).addClass('modal-open');
+          $(document.body).addClass("modal-open");
         }, 0);
       }
     }); //테이블 내 체크박스 전체 선택
 
-    $('[data-event="checkAll"]').on('change', function (e) {
+    $('[data-event="checkAll"]').on("change", function (e) {
       e.preventDefault();
-      var target = $(this).attr('data-target');
+      var target = $(this).attr("data-target");
 
-      if ($(this).is(':checked')) {
-        $('[name=' + target + ']').prop('checked', true);
+      if ($(this).is(":checked")) {
+        $("[name=" + target + "]").prop("checked", true);
       } else {
-        $('[name=' + target + ']').prop('checked', false);
+        $("[name=" + target + "]").prop("checked", false);
       }
     }); //입력폼 max 체크
 
-    $('textarea, input').on("input", function () {
-      if ($(this).attr('maxlength') !== "") {
+    $("textarea, input").on("input", function () {
+      if ($(this).attr("maxlength") !== "") {
         var maxlength = $(this).attr("maxlength");
         var content = $(this).val();
-        $($(this).attr('data-byte-target')).html(content.length);
+        $($(this).attr("data-byte-target")).html(content.length);
 
         if (content.length > maxlength) {
           $(this).val(content.substring(0, maxlength));
-          $($(this).attr('data-byte-target')).html();
+          $($(this).attr("data-byte-target")).html();
         }
       }
     }); //accordion style
 
-    $('.list-group-toggle a.list-group-item').on('click', function (e) {
+    $(".list-group-toggle a.list-group-item").on("click", function (e) {
       e.preventDefault();
-      $(this).closest('.item-wrap').siblings('.item-wrap').removeClass('active');
-      $(this).closest('.item-wrap').addClass('active');
+      $(this).closest(".item-wrap").siblings(".item-wrap").removeClass("active");
+      $(this).closest(".item-wrap").addClass("active");
     });
   }
 };
